db3126872ba9dc0035a00d826d50dcc89ae2f960
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-25 10:51:55 -0700

    initital setup for server

---
+++ api/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { PORT } = require("./config");
+const { NotFoundError } = require("./utils/errors");
+// const security = require("./middleware/security")
+// const authRoutes = require("./routes/auth")
+// const orderRoutes = require("./routes/orders")
+// const storeRoutes = require("./routes/store")
+
+const app = express();
+
+
+app.use(cors());
+// parse incoming requests with JSON payloads
+app.use(express.json());
+// log requests info
+app.use(morgan("tiny"));
+
+// // for every request, check if a token exists in the authorization header
+// // if it does, attach the decoded user to res.locals
+// //  - middleware...
+// app.use(security.extractUserFromJwt)
+
+// app.use("/auth", authRoutes);
+// app.use("/orders", orderRoutes);
+// app.use("/store", storeRoutes);
+
+
+/** Handle 404 errors -- this matches everything */
+app.use((req, res, next) => {
+  return next(new NotFoundError())
+})
+
+/** Generic error handler; anything unhandled goes here. */
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message
+
+  return res.status(status).json({
+    error: { message, status },
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+})
