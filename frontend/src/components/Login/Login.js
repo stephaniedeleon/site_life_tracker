b3ec71a42c54fcf336896f814c96e05ee366a313
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-28 10:39:06 -0700

    login page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,8 +1,30 @@
+import "./Login.css";
+
 export default function Login() {
 
     return (
       <div className="Login">
-          Login
+          <div className="card">
+              <h2>Login</h2>
+              <br/>
+
+              <div className='form'>
+                <div className='input-field'>
+                  <label htmlFor='email'>Email</label>
+                  <input type='email' name='email' placeholder='user@gmail.com'/>
+                </div>
+                <div className='input-field'>
+                  <label htmlFor='password'>Password</label>
+                  <input type='password' name='password' placeholder='password'/>
+                </div>
+                <button className='login-btn'>Login</button>
+              </div>
+
+              <div class="footer">
+                <p>Don't have an account? Sign up <a href="/register">here.</a></p>
+              </div>
+          </div>
       </div>
     )
 }
+
