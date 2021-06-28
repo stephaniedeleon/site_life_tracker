8eeb440f50cdcb0ddda6889284bccf344b44c9ed
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-28 15:27:21 -0700

    added hero img in home

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,18 @@
 import "./Home.css";
+import hero from "../../images/hero.jpeg";
+
 
 export default function Home() {
 
-    return (
-      <div className="Home">
-          Home = what all visitors can see including logged out users...
-          Life Tracker = Helping you take back control of your world!
+  return (
+    <div className="Home">
+      <div class="summary">
+        <div class="text">
+            <h1>LifeTracker</h1>
+            <p>Helping you take back control of your world!</p>
+        </div>
+        <div class="media"><img src={hero} alt="a person with a fitbit" /></div>
       </div>
-    )
+    </div>
+  )
 }
