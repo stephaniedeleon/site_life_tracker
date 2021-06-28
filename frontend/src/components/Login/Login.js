62672ce1a5c6fb03a80314baf171f2f142d37093
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-28 14:05:36 -0700

    removed useEffect

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom"
 import apiClient from "../../services/apiClient";
 import "./Login.css";
