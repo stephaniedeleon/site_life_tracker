40de1b8b5ae562f19582d721ad63bb60946517d4
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-28 19:29:37 -0700

    class to className

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -6,12 +6,12 @@ export default function Home() {
 
   return (
     <div className="Home">
-      <div class="summary">
-        <div class="text">
+      <div className="summary">
+        <div className="text">
             <h1>LifeTracker</h1>
             <p>Helping you take back control of your world!</p>
         </div>
-        <div class="media"><img src={hero} alt="a person with a fitbit" /></div>
+        <div className="media"><img src={hero} alt="a person with a fitbit" /></div>
       </div>
     </div>
   )
