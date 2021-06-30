4fa418c2c4034f60452c2bee1521186050c8a4bc
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-06-30 10:50:21 -0700

    fixed absolute import bug

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,17 +1,17 @@
-export {default as Navbar } from "../Navbar/Navbar";
-export {default as Home } from "../Home/Home";
+export {default as Navbar } from "./Navbar/Navbar";
+export {default as Home } from "./Home/Home";
 
-export {default as Activity } from "../Activity/Activity";
+export {default as Activity } from "./Activity/Activity";
 
-export {default as Exercise } from "../Exercise/Exercise";
-export {default as CreateExercise } from "../CreateExercise/CreateExercise";
+export {default as Exercise } from "./Exercise/Exercise";
+export {default as CreateExercise } from "./CreateExercise/CreateExercise";
 
-export {default as Nutrition } from "../Nutrition/Nutrition";
-export {default as RecordNutrition } from "../RecordNutrition/RecordNutrition";
+export {default as Nutrition } from "./Nutrition/Nutrition";
+export {default as RecordNutrition } from "./RecordNutrition/RecordNutrition";
 
-export {default as Sleep } from "../Sleep/Sleep";
+export {default as Sleep } from "./Sleep/Sleep";
 
-export {default as Login } from "../Login/Login";
-export {default as SignUp } from "../SignUp/SignUp";
+export {default as Login } from "./Login/Login";
+export {default as SignUp } from "./SignUp/SignUp";
 
-export {default as PageHeader } from "../PageHeader/PageHeader";
+export {default as PageHeader } from "./PageHeader/PageHeader";
