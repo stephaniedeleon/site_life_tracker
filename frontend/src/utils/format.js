ca791ee0bdc1fb2a2d2faefe31f6f7ce4af43029
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-07-01 14:41:18 -0700

    added date and time formats for sleep

--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -4,3 +4,13 @@ export const formatDate = (date) => {
   const d = new Date(date)
   return moment(d).format("MMM DD [at] h:mm a")
 }
+
+export const formatTime = (date) => {
+  const d = new Date(date)
+  return moment(d).format("h:mm a")
+}
+
+export const formatSleepDate = (date) => {
+  const d = new Date(date)
+  return moment(d).subtract(1, "days").format("MMM Do[,] YYYY")
+}
