ed4d1e009f503711d7185afbdc87e51641cbc87e
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-07-01 21:06:17 -0700

    updated styles and home page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,18 +1,39 @@
+import { Link } from "react-router-dom";
 import "./Home.css";
-import hero from "images/hero.jpeg";
-
 
 export default function Home() {
 
   return (
     <div className="Home">
-      <div className="summary">
-        <div className="text">
-            <h1>LifeTracker</h1>
-            <p>Helping you take back control of your world!</p>
-        </div>
-        <div className="media"><img src={hero} alt="a person with a fitbit" /></div>
+
+      <div className="tiles">
+        <Link to="/exercise" className="tile" >
+            <img src="http://codepath-lifetracker.surge.sh/static/media/icons-workout-48.4f4cdb05.svg" alt="workout icon" />
+            <p>Fitness</p>
+        </Link>
+        <Link to="/nutrition" className="tile">
+            <img src="http://codepath-lifetracker.surge.sh/static/media/icons8-porridge-100.132d2715.svg" alt="porridge icon" />
+            <p>Food</p>
+        </Link>
       </div>
+
+      <div className="hero">
+        <img src="http://codepath-lifetracker.surge.sh/static/media/smartwatch-screen-digital-device.e2983a85.svg" alt="a fitbit" />
+        <h1>LifeTracker</h1>
+        <p>Helping you take back control of your world!</p>
+      </div>
+
+      <div className="tiles">
+        <Link to="/sleep" className="tile">
+            <img src="http://codepath-lifetracker.surge.sh/static/media/icons8-resting-100.81067336.svg" alt="person on a lazy boy icon" />
+            <p>Rest</p>
+        </Link>
+        <Link to="/activity" className="tile">
+            <img src="http://codepath-lifetracker.surge.sh/static/media/icons8-planner-100.997ca54c.svg" alt="calendar icon" />
+            <p>Planner</p>
+        </Link>
+      </div>
+
     </div>
   )
 }
